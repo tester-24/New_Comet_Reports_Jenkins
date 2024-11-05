4117f420f30050d9c06d35ece6ddb8109f472ddd
Tester1 <137996855+tester-24@users.noreply.github.com>
2024-11-05 11:20:05 +0530

    New Comet Reports.

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,7 @@ module.exports = defineConfig({
   reporterOptions: {
    
     charts: true,
+    video: true,
     reportPageTitle: 'custom-title',
     embeddedScreenshots: true,
     inlineAssets: true,
