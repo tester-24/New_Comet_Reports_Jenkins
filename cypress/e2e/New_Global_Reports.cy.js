fcd2bc224b8c959124b60509f5533e34bcca83c2
Tester1 <137996855+tester-24@users.noreply.github.com>
2024-12-06 13:50:11 +0530

    New Comet Reports.

--- cypress/e2e/New_Global_Reports.cy.js
+++ cypress/e2e/New_Global_Reports.cy.js
@@ -177,8 +177,8 @@ it('New_Global_Report_Segment_Derivative', () => {
       cy.scrollTo('top')
       //Select Excel type
       cy.xpath('/html[1]/body[1]/app-root[1]/kendo-popup[1]/div[1]/kendo-list[1]/div[1]/ul[1]/li[2]').should('be.visible')
-      cy.wait(500)
-      cy.xpath('/html[1]/body[1]/app-root[1]/kendo-popup[1]/div[1]/kendo-list[1]/div[1]/ul[1]/li[2]').click()
+      cy.wait(1000)
+      cy.xpath('/html[1]/body[1]/app-root[1]/kendo-popup[1]/div[1]/kendo-list[1]/div[1]/ul[1]/li[2]').click({force:true})
       cy.scrollTo('top')
        //Click on download button
        cy.wait(1000)
