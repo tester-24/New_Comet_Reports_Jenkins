7731964156b0ff7cc337a8f6c8d2c3bb0c49b328
Tester1 <137996855+tester-24@users.noreply.github.com>
2025-01-30 18:36:39 +0530

    New Comet Reports

--- cypress/e2e/New_Tradelog_Reports.cy.js
+++ cypress/e2e/New_Tradelog_Reports.cy.js
@@ -8,11 +8,24 @@ describe('New_Tradelog_ReportS ',() =>
 it('New_Tradelog_Reports_FY_2023-2024', () => 
    {
     cy.visit('https://comet.jainam.in/#/startup')
-    cy.Login2();    
+    cy.Login2();  
+    cy.xpath("//a[@class='nav_title text_decoration_none d_inline_block']//img[@alt='reports']").click({force:true})
+    // cy.wait(500)
+     cy.xpath("//a[@class='nav_title text_decoration_none d_inline_block']//img[@alt='reports']").should('be.visible')
+     cy.wait(500)
+     cy.window().then((win) => {
+            cy.stub(win, 'open').callsFake((url) => {
+              win.location.href = url;
+            });
+          });
+          cy.wait(2000)
+       //  cy.xpath('/html/body/app-root/app-layout/div/div/app-dashboard/div[2]/div/app-invest-with-us/div/a[4]').click()
+     cy.xpath('/html[1]/body[1]/app-root[1]/app-layout[1]/app-client-header-navbar[1]/div[1]/div[1]/nav[1]/div[2]/ul[1]/li[3]/div[1]/ul[1]/li[2]/a[1]').click({force:true})
+
     //Select Tradelog
-    cy.wait(500)
-    cy.get('[href="#/tradelog"]').click({force:true})
-    cy.wait(1000)
+    //cy.wait(500)
+   // cy.get('[href="#/tradelog"]').click({force:true})
+   // cy.wait(1000)
     cy.scrollTo('top')
    //Reports for Year 2023-2024
      //Click on year dropdown
@@ -40,9 +53,18 @@ it('New_Tradelog_Reports_FY_2023-2024', () =>
 it('New_Tradelog_Reports_FY_2024-2025', () => {
   cy.Login2();
   //Select Tradelog
-  cy.wait(500)
-  cy.get('[href="#/tradelog"]').click({force:true})
-  cy.wait(1000)
+  cy.xpath("//a[@class='nav_title text_decoration_none d_inline_block']//img[@alt='reports']").click({force:true})
+    // cy.wait(500)
+     cy.xpath("//a[@class='nav_title text_decoration_none d_inline_block']//img[@alt='reports']").should('be.visible')
+     cy.wait(500)
+     cy.window().then((win) => {
+            cy.stub(win, 'open').callsFake((url) => {
+              win.location.href = url;
+            });
+          });
+          cy.wait(2000)
+       //  cy.xpath('/html/body/app-root/app-layout/div/div/app-dashboard/div[2]/div/app-invest-with-us/div/a[4]').click()
+       cy.xpath('/html[1]/body[1]/app-root[1]/app-layout[1]/app-client-header-navbar[1]/div[1]/div[1]/nav[1]/div[2]/ul[1]/li[3]/div[1]/ul[1]/li[2]/a[1]').click({force:true})
   cy.scrollTo('top')
   //Click on year dropdown
   cy.xpath('/html/body/app-root/app-layout/div/app-tradelog/div/form/div[2]/div[1]/kendo-dropdownlist/button').click()
