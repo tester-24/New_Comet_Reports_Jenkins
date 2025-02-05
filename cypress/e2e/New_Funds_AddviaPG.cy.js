26466a4f2eec9f03d5190f413473beaf5dead917
Tester1 <137996855+tester-24@users.noreply.github.com>
2025-02-05 11:10:13 +0530

    New Comet Reports

--- cypress/e2e/New_Funds_AddviaPG.cy.js
+++ cypress/e2e/New_Funds_AddviaPG.cy.js
@@ -29,23 +29,25 @@ it('Comet_Jainam', () => {
     cy.get('#pin3').type(3)
     cy.get('#pin4').type(4)
     cy.wait(5000)
+    // click on funds 
+    cy.get(':nth-child(3) > .nav-link').click({force:true})
    // cy.xpath("/html[1]/body[1]/app-root[1]/app-layout[1]/app-client-header-navbar[1]/div[1]/div[1]/nav[1]/div[3]/ul[1]/li[2]/a[1]/span[1]/img[2]").click({force:true})
     // cy.wait(500)
    //  cy.xpath("//a[@class='nav_title text_decoration_none d_inline_block']//img[@alt='reports']").should('be.visible')
     // cy.wait(500)
-     cy.window().then((win) => {
-            cy.stub(win, 'open').callsFake((url) => {
-              win.location.href = url;
-            });
-          });
-          cy.wait(2000)
-       //  cy.xpath('/html/body/app-root/app-layout/div/div/app-dashboard/div[2]/div/app-invest-with-us/div/a[4]').click()
-     cy.xpath('/html[1]/body[1]/app-root[1]/app-layout[1]/app-client-header-navbar[1]/div[1]/div[1]/nav[1]/div[3]/ul[1]/li[2]/a[1]/span[1]/img[2]').click({force:true})
+  //    cy.window().then((win) => {
+  //           cy.stub(win, 'open').callsFake((url) => {
+  //             win.location.href = url;
+  //           });
+  //         });
+  //         cy.wait(2000)
+  //      //  cy.xpath('/html/body/app-root/app-layout/div/div/app-dashboard/div[2]/div/app-invest-with-us/div/a[4]').click()
+  //    cy.xpath('/html[1]/body[1]/app-root[1]/app-layout[1]/app-client-header-navbar[1]/div[1]/div[1]/nav[1]/div[3]/ul[1]/li[2]/a[1]/span[1]/img[2]').click({force:true})
     
-   // cy.xpath("/html[1]/body[1]/app-root[1]/app-layout[1]/app-client-header-navbar[1]/div[1]/div[1]/nav[1]/div[3]/ul[1]/li[2]/a[1]/span[1]/img[2]").click({force:true})
-    // cy.wait(500)
-    // cy.xpath("//a[@class='nav_title text_decoration_none d_inline_block']//img[@alt='reports']").should('be.visible')
-    // cy.wait(500)
+  //  // cy.xpath("/html[1]/body[1]/app-root[1]/app-layout[1]/app-client-header-navbar[1]/div[1]/div[1]/nav[1]/div[3]/ul[1]/li[2]/a[1]/span[1]/img[2]").click({force:true})
+  //   // cy.wait(500)
+  //   // cy.xpath("//a[@class='nav_title text_decoration_none d_inline_block']//img[@alt='reports']").should('be.visible')
+  //   // cy.wait(500)
      cy.window().then((win) => {
             cy.stub(win, 'open').callsFake((url) => {
               win.location.href = url;
