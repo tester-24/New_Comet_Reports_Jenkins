bd4259540d1eb7f3016ea7a9d6093c0094b65427
Tester1 <137996855+tester-24@users.noreply.github.com>
2025-05-02 18:38:08 +0530

    New Comet Reports

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -42,7 +42,7 @@ Cypress.Commands.add('Login', (Username,password) => {
       cy.xpath('/html/body/app-root/app-sign-in/div/div/div[2]/div/form/button[1]').click()
       
       // enter password
-      cy.xpath('/html/body/app-root/app-sign-in/div/div/div[2]/app-sign-in-with-username/form/div/div/div/div/kendo-textbox/input').type('Jainam@123')
+      cy.xpath('/html/body/app-root/app-sign-in/div/div/div[2]/app-sign-in-with-username/form/div/div/div/div/kendo-textbox/input').type('J@inam$789')
       
       //click on continue button
       cy.xpath('/html/body/app-root/app-sign-in/div/div/div[2]/app-sign-in-with-username/form/div/button').click()
